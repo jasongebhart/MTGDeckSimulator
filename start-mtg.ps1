ebfbfac69c67edae1cb13eb990055913d2e9a5f5
jasongebhart <jason.gebhart@gmail.com>
2023-08-11 20:36:42 -0700

    add startmtg.js

--- start-mtg.ps1
+++ start-mtg.ps1
@@ -1,2 +1,3 @@
 # Set-Location -Path "$env:userprofile\Documents\MTGDeckSimulator"
 Start-Process -FilePath "$env:programfiles\nodejs\node.exe" -ArgumentList "$PSScriptRoot\startapp"
+Start-Process "firefox.exe" -ArgumentList "http://localhost:3000"
